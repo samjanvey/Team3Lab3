ea3130aecf8d8d6bc8184a8089dd4d9a0b8596aa
sjanvey <sjanvey@169.254.125.81>
2020-02-03 14:58:09 -0500

    Week 3 : Webservices API - NOAA

---
+++ src/team3week3/Team3Week3.java
@@ -0,0 +1,15 @@
+
+package team3week3;
+
+/**
+ *
+ * @author sjanvey
+ */
+public class Team3Week3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
