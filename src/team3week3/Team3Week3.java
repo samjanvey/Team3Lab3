85561dd7763874589dfe272ae88c2b3804ae4983
sjanvey <sjanvey@169.254.125.81>
2020-02-05 18:57:54 -0500

    Receiving 200 but something still causing an error

--- src/team3week3/Team3Week3.java
+++ src/team3week3/Team3Week3.java
@@ -4,7 +4,6 @@ package team3week3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,7 +18,7 @@ import java.net.URL;
  */
 public class Team3Week3 {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws IOException {
         // NOAA Dataset API Endpoint
         // Change this value to any API endpoint
         URL url = new URL("https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets");
@@ -27,25 +26,34 @@ public class Team3Week3 {
         
         try {
             // Open connection to API Endpoint
-            HttpURLConnection connection = (HttpURLConnection) url
-                .openConnection();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setRequestMethod("GET"); // Change to any HTTP method
-            connection.setRequestProperty("Authorization", apiToken);
+            connection.setRequestProperty("Token", apiToken); // Add the API Token to the Request Header
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response: " + responseCode); // 200 == OK
             
-            // Same as Week 1 - Create I/O reader
-            PrintStream out = new PrintStream(connection.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            
-            out.println("GET " + "/" + " HTTP/1.0");
-            out.println();
-            
-            String lineIn = in.readLine();
-            while (lineIn != null) {
-                System.out.println(lineIn);
-                lineIn = in.readLine();
+            if(responseCode == HttpURLConnection.HTTP_OK) {
+                // Same as Week 1 - Create I/O reader
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    String lineIn = in.readLine();
+                    StringBuilder response = new StringBuilder();
+                    while (lineIn != null) {
+                        response.append(lineIn);
+                    }
+                    in.close();
+                
+                    System.out.println(response.toString());
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Something went wrong");
             }
-            
+                
+                 
         } catch(Exception e) {
             e.printStackTrace();
         }
